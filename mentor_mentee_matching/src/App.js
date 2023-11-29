deb44d3961bb740544d5e75353245c016f475988
karthikv6 <44593407+karthikv6@users.noreply.github.com>
2023-11-29 07:28:23 +0530

    landing, login and sign up pages done, no routing

---
+++ mentor_mentee_matching/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import LandingPage from './landing.jsx';
+import SignInPage from './signin.jsx';
+import SignUpPage from './signup.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <SignUpPage /> {}
+    </div>
+  );
+}
+
+export default App;
